5783fb72671a08bce9eb6b987ed32c32398cc9ed
Domien Jansen <domien.jansen@icapps.com>
2021-04-06 11:19:12 +0200

    Install jwt auth bundle + config

--- symfony/config/bundles.php
+++ symfony/config/bundles.php
@@ -15,4 +15,5 @@ return [
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Snc\RedisBundle\SncRedisBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
