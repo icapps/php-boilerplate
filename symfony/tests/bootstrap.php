18ac6be93b3664eb4e8bd3b74861fe7bdf04a638
Domien Jansen <domien.jansen@icapps.com>
2021-04-09 17:20:39 +0200

    Merge with master pt.2

--- symfony/tests/bootstrap.php
+++ symfony/tests/bootstrap.php
@@ -1,11 +1,15 @@
 <?php
 
+/*
+ * This file is part of Sulu.
+ *
+ * (c) Sulu GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 use Symfony\Component\Dotenv\Dotenv;
 
-require dirname(__DIR__).'/vendor/autoload.php';
-
-if (file_exists(dirname(__DIR__).'/config/bootstrap.php')) {
-    require dirname(__DIR__).'/config/bootstrap.php';
-} elseif (method_exists(Dotenv::class, 'bootEnv')) {
-    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
-}
+require \dirname(__DIR__) . '/vendor/autoload.php';
+(new Dotenv())->bootEnv(\dirname(__DIR__) . '/.env');
